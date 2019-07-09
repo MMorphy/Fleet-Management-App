398716bee56444baecf0a130da38e2d1842dd2ff
Tomislav Petkovic <tomislav.petkovic@ericsson.com>
2019-07-09 14:21:06 +0200

    Initial commit 	*Skeleton

---
+++ src/test/java/hr/petkovic/fleet/FleetManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.petkovic.fleet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FleetManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
