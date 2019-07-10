b223519aa37c6d692088e99461c210bcbbcab042
Tomislav Petkovic <tomislav.petkovic@cromtg.com>
2019-07-10 14:38:12 +0200

    Completed project structure

--- src/main/java/hr/petkovic/fleet/FleetManagementApplication.java
+++ src/main/java/hr/petkovic/fleet/FleetManagementApplication.java
@@ -2,7 +2,9 @@ package hr.petkovic.fleet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("hr.petkovic.fleet.repositories")
 @SpringBootApplication
 public class FleetManagementApplication {
 
